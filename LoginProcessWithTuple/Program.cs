70129f618631d764fa839c8997ca518091e0698d
Hajibayov <shacibyov@gmail.com>
2023-12-15 03:48:27 +0400

    commit

---
+++ LoginProcessWithTuple/Program.cs
@@ -0,0 +1,27 @@
+﻿(string, int) account = ("admin", 123456);
+bool loggedIn = true;
+
+while (loggedIn)
+{
+    Console.Write("Enter your username:");
+    string userName = Console.ReadLine();
+    Console.Write("Enter your password:");
+    int password = int.Parse(Console.ReadLine());
+
+    try
+    {
+        if (userName == account.Item1 && password == account.Item2)
+        {
+            loggedIn = false;
+        }
+        else
+        {
+            throw new Exception("Wrong username or password");
+        }
+    }
+    catch(Exception ex) 
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+}
